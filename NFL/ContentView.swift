1f5f2dd64b3b953b9ed4da0e4efef4dfc0604de9
John Harold Rasco <john.harold.rasco@codeandtheory.com>
2022-05-13 12:55:53 +0800

    Initial commit.

---
+++ NFL/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NFL
+//
+//  Created by John Harold Rasco on 5/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NFL/NFLApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFLApp.swift
+//  NFL
+//
+//  Created by John Harold Rasco on 5/13/22.
+//
+
+import SwiftUI
+
+@main
+struct NFLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
